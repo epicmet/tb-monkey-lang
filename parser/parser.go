2694be4bb9634c7e565cf6ad08998e803ca19daa
Mahdi Aghaei <mahd.aghaei@gmail.com>
2024-10-09 20:14:10 +0330

    Parse return statement & prepare to parse expressions

--- parser/parser.go
+++ parser/parser.go
@@ -77,6 +77,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.currToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+  case token.RETURN:
+    return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -101,4 +103,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	}
 
 	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+  stmt := &ast.ReturnStatement{Token: p.currToken}
+
+  p.nextToken()
+
+  // TODO: Parse expression
+  for !p.currTokenIs(token.SEMICOLON) {
+    p.nextToken()
+  }
+
+  return stmt
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -17,7 +17,7 @@ let foobar = 838383;
 	p := parser.New(l)
 
 	program := p.ParseProgram()
-  checkParserErrors(t, p)
+	checkParserErrors(t, p)
 	if program == nil {
 		t.Fatalf("ParseProgram() returned nil")
 	}
@@ -42,6 +42,36 @@ let foobar = 838383;
 	}
 }
 
+func TestReturnStatements(t *testing.T) {
+	input := `
+return 5;
+return 10;
+return 993322;
+  `
+
+	l := lexer.New(input)
+	p := parser.New(l)
+
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements does not contain 3 statements. got=%d", len(program.Statements))
+	}
+
+	for _, stmt := range program.Statements {
+		returnStmt, ok := stmt.(*ast.ReturnStatement)
+		if !ok {
+			t.Errorf("stmt is not *ast.ReturnStatement. got=%T", stmt)
+			continue
+		}
+
+		if returnStmt.TokenLiteral() != "return" {
+			t.Errorf("returnStmt.TokenLiteral() is not 'return'. got %q", returnStmt.TokenLiteral())
+		}
+	}
+}
+
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
@@ -51,33 +81,33 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	letStmt, ok := s.(*ast.LetStatement)
 	if !ok {
 		t.Errorf("s not *ast.LetStatement. got=%T", s)
-    return false
+		return false
 	}
 
-  if letStmt.Name.Value != name {
-    t.Errorf("s.Name.Value is not '%s'. got=%s", name, letStmt.Name.Value)
-    return false
-  }
+	if letStmt.Name.Value != name {
+		t.Errorf("s.Name.Value is not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
 
-  if letStmt.Name.TokenLiteral() != name {
-    t.Errorf("s.Name is not '%s'. got=%s", name, letStmt.Name)
-    return false
-  }
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name is not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
 
 	return true
 }
 
 func checkParserErrors(t *testing.T, p *parser.Parser) {
-  errors := p.Errors()
+	errors := p.Errors()
 
-  if len(errors) == 0 {
-    return
-  }
+	if len(errors) == 0 {
+		return
+	}
 
-  t.Errorf("parser has %d errors", len(errors))
-  for _, msg := range errors {
-    t.Errorf("Parser error: %q", msg)
-  }
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("Parser error: %q", msg)
+	}
 
-  t.FailNow()
+	t.FailNow()
 }
